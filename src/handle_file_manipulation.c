3076019a3a8df3c42f0208ee9d3ada62757963b6
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-09-19 10:21:19 +0100

    feat: Adds function to handle file's 'opening' and 'closing'.

---
+++ src/handle_file_manipulation.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_file_manipulation.c                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 09:06:16 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/09/19 10:06:52 by arabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+int	handle_open(char *file_name)
+{
+	int	fd;
+
+	fd = open(file_name, O_RDONLY);
+	if (fd == -1)
+	{
+		ft_putstr_fd("Error: File can't be opened, it might not exists! ", 2);
+		ft_putendl_fd("Exiting...", 2);
+		exit(EXIT_FAILURE);
+	}
+	return (fd);
+}
+
+void	handle_close(int fd, char **matrix)
+{
+	if (close(fd) == -1)
+	{
+		free_matrix(matrix);
+		ft_putstr_fd("Error: File can't be closed, it might not exists! ", 2);
+		ft_putendl_fd("Exiting...", 2);
+		exit(EXIT_FAILURE);
+	}
+}
