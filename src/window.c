10af6f76bd05a75685b9925f71ec31295c3b5189
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-09-28 18:03:00 +0100

    fix: Norm Error with window.c

--- src/window.c
+++ src/window.c
@@ -6,19 +6,25 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 11:18:27 by arabelo-          #+#    #+#             */
-/*   Updated: 2023/09/27 18:20:52 by arabelo-         ###   ########.fr       */
+/*   Updated: 2023/09/28 17:46:02 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+void	window_init(void)
+{
+	map()->mlx = mlx_init();
+	mlx_get_screen_size(map()->mlx, &map()->win_w, &map()->win_h);
+}
 
 void	put_pixel(int x, int y, int color)
 {
 	char	*dst;
 
 	dst = map()->addr + (y * map()->columns_amount
-		+ x * (map()->bpp / 8));
-	*(unsigned int*)dst = color;
+			+ x * (map()->bpp / 8));
+	*(unsigned int *)dst = color;
 }
 
 void	put_pixels_on_img(void)
@@ -42,16 +48,17 @@ void	put_pixels_on_img(void)
 
 void	window(void)
 {
-	map()->mlx = mlx_init();
-	mlx_get_screen_size(map()->mlx, &map()->win_w, &map()->win_h);
-	map()->img = mlx_new_image(map()->mlx, map()->win_w, map()->win_h);
-	map()->mlx_win = mlx_new_window(map()->mlx, map()->win_w,
-		map()->win_h, "fdf");
-	map()->addr = mlx_get_data_addr(map()->img, &map()->bpp, &map()->columns_amount,
-			&map()->endian);
+	t_node	*map;
+
+	map = map;
+	map->img = mlx_new_image(map->mlx, map->win_w, map->win_h);
+	map->mlx_win = mlx_new_window(map->mlx, map->win_w,
+			map->win_h, "fdf");
+	map->addr = mlx_get_data_addr(map->img, &map->bpp, &map->columns_amount,
+			&map->endian);
 	put_pixels_on_img();
-	mlx_put_image_to_window(map()->mlx, map()->mlx_win, map()->img, 0, 0);
-	mlx_hook(map()->mlx_win, ON_KEYDOWN, 1L<<0, esc_window, map());
-	mlx_hook(map()->mlx_win, ON_DESTROY, 0, mouse_destroy_window, map());
-	mlx_loop(map()->mlx);
+	mlx_put_image_to_window(map->mlx, map->mlx_win, map->img, 0, 0);
+	mlx_hook(map->mlx_win, ON_KEYDOWN, 1L << 0, esc_window, map);
+	mlx_hook(map->mlx_win, ON_DESTROY, 0, mouse_destroy_window, map);
+	mlx_loop(map->mlx);
 }
