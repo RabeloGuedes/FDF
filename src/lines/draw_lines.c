298679c4c536a13b0e7a00188b0bbd44a89e0495
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-10-13 14:57:35 +0100

    fix: Line gradient coloring.

--- src/lines/draw_lines.c
+++ src/lines/draw_lines.c
@@ -6,16 +6,30 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 14:10:25 by arabelo-          #+#    #+#             */
-/*   Updated: 2023/10/11 15:06:15 by arabelo-         ###   ########.fr       */
+/*   Updated: 2023/10/13 14:56:33 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+int line_color(int first_color, int second_color, float delta)
+{
+	int color[3];
+
+	color[0] = (float)(first_color >> 16 & 0xFF) * (1.0f - delta)
+		+ (float)(second_color >> 16 & 0xFF) * delta;
+	color[1] = (float)(first_color >> 8 & 0xFF) * (1.0f - delta)
+		+ (float)(second_color >> 8 & 0xFF) * delta;
+	color[2] = (float)(first_color & 0xFF) * (1.0f - delta)
+		+ (float)(second_color & 0xFF) * delta;
+	return (color[0] << 16 | color[1] << 8 | color[2]);
+}
+
 void	draw_line(t_node *first, t_node *second)
 {
 	double	steps;
 	double	max_steps;
+	int		pixel_color;
 
 	map()->coor->delta_x = second->x - first->x;
 	map()->coor->delta_y = second->y - first->y;
@@ -29,7 +43,8 @@ void	draw_line(t_node *first, t_node *second)
 	steps = 0;
 	while (++steps <= max_steps)
 	{
-		put_pixel(map()->coor->x, map()->coor->y, first->color);
+		pixel_color = line_color(first->color, second->color, steps / max_steps);
+		put_pixel(map()->coor->x, map()->coor->y, pixel_color);
 		map()->coor->x += (map()->coor->delta_x / max_steps);
 		map()->coor->y += (map()->coor->delta_y / max_steps);
 	}
